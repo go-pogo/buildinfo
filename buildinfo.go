8db2ba6ae5b4a53161e5e37320ca2a4f3106553f
roeldev <roeldev@users.noreply.github.com>
2021-11-13 15:09:21 +0100

    Update BuildInfo

--- buildinfo.go
+++ buildinfo.go
@@ -1,12 +1,61 @@
+// Copyright (c) 2020, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package buildinfo provides basic building blocks and instructions to easily add
+build and release information to your app.
+
+	var (
+		version = buildinfo.DummyVersion
+		revision = buildinfo.DummyRevision
+		date = buildinfo.DummyDate
+	)
+
+	func main() {
+		bld := buildinfo.BuildInfo{
+			Version:  version,
+			Revision: revision,
+			Date:     date,
+		}
+	}
+
+Build your Go project and include the following ldflags:
+
+	go build -ldflags=" \
+	  -X main.version=`$(git describe --tags)` \
+	  -X main.revision=`$(git rev-parse --short HEAD)` \
+	  -X main.date=`$(date +%FT%T%z`)" \
+	  main.go
+
+Prometheus metric collector
+
+When using a metrics scraper like Prometheus, it is often a good idea to make
+the build information of your app available. Below example shows just how easy
+it is to create and register a collector with the build information as
+constant labels.
+
+	prometheus.MustRegister(prometheus.NewGaugeFunc(
+	    prometheus.GaugeOpts{
+	        Namespace:   "myapp",
+	        Name:        buildinfo.MetricName,
+	        Help:        buildinfo.MetricHelp,
+	        ConstLabels: bld.Map(),
+	    },
+	    func() float64 { return 1 },
+	))
+
+*/
 package buildinfo
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 	"runtime"
+	"strings"
 )
 
-//noinspection GoUnusedConst
+//goland:noinspection GoUnusedConst
 const (
 	// ShortFlag is the default flag to print the current build information of the app.
 	ShortFlag = "v"
@@ -19,51 +68,20 @@ const (
 	MetricHelp = "Metric with build information labels and a constant value of '1'."
 )
 
-// BuildInfo
+// BuildInfo contains the relevant information of the current release's build
+// version, revision and build date.
 type BuildInfo struct {
-	Version  string
+	// Version is the current version of the release.
+	Version string
+	// Revision is the (short) commit hash the release is build from.
 	Revision string
-	Branch   string
-	Date     string
+	// Date of when the release was build.
+	Date string
 }
 
-// GoVersion returns the version of the used Go runtime. See `runtime.Version()`
+// GoVersion returns the version of the used Go runtime. See runtime.Version
 // for additional details.
 func (bld BuildInfo) GoVersion() string { return runtime.Version() }
-
-// String returns the string representation of the build information.
-// It always includes the release version. Other fields are omitted when empty.
-// Examples:
-//  - version only: `v8.0.0`
-//  - version and branch: `3.0.2 stable`
-//  - all without branch: `v8.5.0 (rev fedcba, date 2020-06-16 19:53)`
-//  - all: `1.0.1 dev (rev fedcba, date 1997-08-29 13:37:00)`
-func (bld BuildInfo) String() string {
-	branch := bld.Branch
-	if branch != "" {
-		branch = " " + branch
-	}
-
-	if bld.Revision == "" && bld.Date == "" {
-		return bld.Version + branch
-	}
-
-	var buf bytes.Buffer
-	if bld.Revision != "" {
-		buf.WriteString("rev ")
-		buf.WriteString(bld.Revision)
-	}
-	if bld.Date != "" {
-		if buf.Len() != 0 {
-			buf.WriteString(", ")
-		}
-
-		buf.WriteString("date ")
-		buf.WriteString(bld.Date)
-	}
-
-	return fmt.Sprintf("%s%s (%s)", bld.Version, branch, buf.String())
-}
 
 // Map returns the build information as a map. Field names are lowercase.
 // Empty fields within BuildInfo are omitted.
@@ -72,33 +90,26 @@ func (bld BuildInfo) Map() map[string]string {
 	m["version"] = bld.Version
 
 	if bld.Revision != "" {
-		m["rev"] = bld.Revision
-	}
-	if bld.Branch != "" {
-		m["branch"] = bld.Branch
+		m["revision"] = bld.Revision
 	}
 	if bld.Date != "" {
 		m["date"] = bld.Date
 	}
 
-	m["goversion"] = runtime.Version()
+	m["goversion"] = bld.GoVersion()
 	return m
 }
 
 // MarshalJSON returns valid JSON output.
 // Empty fields within BuildInfo are omitted.
 func (bld BuildInfo) MarshalJSON() ([]byte, error) {
-	var buf bytes.Buffer
+	var buf strings.Builder
 	buf.WriteString(`{"version":"`)
 	buf.WriteString(bld.Version)
 
 	if bld.Revision != "" {
-		buf.WriteString(`","rev":"`)
+		buf.WriteString(`","revision":"`)
 		buf.WriteString(bld.Revision)
-	}
-	if bld.Branch != "" {
-		buf.WriteString(`","branch":"`)
-		buf.WriteString(bld.Branch)
 	}
 	if bld.Date != "" {
 		buf.WriteString(`","date":"`)
@@ -106,25 +117,95 @@ func (bld BuildInfo) MarshalJSON() ([]byte, error) {
 	}
 
 	buf.WriteString(`","goversion":"`)
-	buf.WriteString(runtime.Version())
+	buf.WriteString(bld.GoVersion())
 	buf.WriteString(`"}`)
 
-	return buf.Bytes(), nil
+	return []byte(buf.String()), nil
+}
+
+// String returns the string representation of the build information.
+// It always includes the release version. Other fields are omitted when empty.
+// Examples:
+//  - version only: `v8.0.0`
+//  - version and revision `v8.5.0 (fedcba)`
+//  - version and date: `v8.5.0 (2020-06-16 19:53)`
+//  - all: `v8.5.0 (fedcba @ 2020-06-16 19:53)`
+func (bld BuildInfo) String() string {
+	if bld.Revision == "" && bld.Date == "" {
+		return bld.Version
+	}
+
+	var buf strings.Builder
+	bld.WriteTo(&buf)
+	return buf.String()
+}
+
+func (bld BuildInfo) Format(s fmt.State, v rune) {
+	switch v {
+	case 'V':
+		if s.Flag('#') {
+			bld.WriteTo(s)
+		} else {
+			s.Write([]byte(bld.Version))
+		}
+
+	case 'R':
+		if bld.Revision != "" {
+			s.Write([]byte(bld.Revision))
+		}
+
+	case 'D':
+		if bld.Date != "" {
+			s.Write([]byte(bld.Date))
+		}
+
+	case 'G':
+		s.Write([]byte(bld.GoVersion()))
+	}
+}
+
+// WriteTo writes a string representation of the build information, similar to
+// String, to w. The return value is the number of bytes written. Any error
+// encountered during writing is ignored.
+func (bld BuildInfo) WriteTo(w io.Writer) (int64, error) {
+	c := countingWriter{target: w}
+	c.Write([]byte(bld.Version))
+
+	if bld.Revision != "" {
+		c.Write([]byte(" (" + bld.Revision))
+		if bld.Date != "" {
+			c.Write([]byte(" @ " + bld.Date))
+		}
+		c.Write([]byte(")"))
+	} else if bld.Date != "" {
+		c.Write([]byte(" (" + bld.Date + ")"))
+	}
+
+	return c.size, nil
+}
+
+type countingWriter struct {
+	target io.Writer
+	size   int64
+}
+
+func (cw *countingWriter) Write(data []byte) (int, error) {
+	n, _ := cw.target.Write(data)
+	cw.size += int64(n)
+	return n, nil
 }
 
 const (
 	DummyVersion  = "0.0.0"
 	DummyRevision = "abcdef"
-	DummyBranch   = "dev"
 	DummyDate     = "1997-08-29 13:37:00"
 )
 
-// IsDummy returns `true` when all fields' values within a `BuildInfo` are dummy
+// IsDummy returns true when all fields' values within a BuildInfo are dummy
 // values. This may indicate the build information variables are not properly
-// overwritten when a new build is made.
+// set when a new build is made.
 func IsDummy(bld BuildInfo) bool {
 	return bld.Version == DummyVersion &&
 		bld.Revision == DummyRevision &&
-		bld.Branch == DummyBranch &&
 		bld.Date == DummyDate
 }
--- buildinfo_test.go
+++ buildinfo_test.go
@@ -9,46 +9,58 @@ import (
 )
 
 func TestBuildInfo_GoVersion(t *testing.T) {
-	assert.Exactly(t, runtime.Version(), BuildInfo{}.GoVersion())
+	assert.Exactly(t, runtime.Version(), new(BuildInfo).GoVersion())
 }
 
 func TestBuildInfo_String(t *testing.T) {
-	t.Run("only version", func(t *testing.T) {
-		want := "v0.12.1"
-		assert.Exactly(t, want, BuildInfo{Version: want}.String())
-	})
-	t.Run("version and branch", func(t *testing.T) {
-		assert.Exactly(t, "3.0.2 stable", BuildInfo{Version: "3.0.2", Branch: "stable"}.String())
-	})
-	t.Run("no branch", func(t *testing.T) {
-		assert.Exactly(t, "v1.0.66 (rev fedcba, date 2020-06-16 19:53)", BuildInfo{
-			Version:  "v1.0.66",
-			Revision: "fedcba",
-			Date:     "2020-06-16 19:53",
-		}.String())
-	})
-	t.Run("all", func(t *testing.T) {
-		assert.Exactly(t, "v1.0.66 develop (rev fedcba, date 2020-06-16 19:53)", BuildInfo{
-			Version:  "v1.0.66",
-			Revision: "fedcba",
-			Branch:   "develop",
-			Date:     "2020-06-16 19:53",
-		}.String())
-	})
+	tests := map[string]struct {
+		input BuildInfo
+		want  string
+	}{
+		"version only": {
+			input: BuildInfo{Version: "v0.12.1"},
+			want:  "v0.12.1",
+		},
+		"version and revision": {
+			input: BuildInfo{
+				Version:  "v1.0.66",
+				Revision: "fedcba",
+			},
+			want: "v1.0.66 (fedcba)",
+		},
+		"version and date": {
+			input: BuildInfo{
+				Version: "0.0.2-rc1",
+				Date:    "2020-06-16 19:53",
+			},
+			want: "0.0.2-rc1 (2020-06-16 19:53)",
+		},
+		"all": {
+			input: BuildInfo{
+				Version:  "v1.0.66",
+				Revision: "fedcba",
+				Date:     "2020-06-16 19:53",
+			},
+			want: "v1.0.66 (fedcba @ 2020-06-16 19:53)",
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Exactly(t, tc.want, tc.input.String())
+		})
+	}
 }
 
 func TestBuildInfo_Map(t *testing.T) {
 	have := BuildInfo{
 		Version:  "v1.0.66",
 		Revision: "fedcba",
-		Branch:   "foobar",
 		Date:     DummyDate,
 	}
 
 	want := map[string]string{
 		"version":   "v1.0.66",
-		"rev":       "fedcba",
-		"branch":    "foobar",
+		"revision":  "fedcba",
 		"date":      DummyDate,
 		"goversion": runtime.Version(),
 	}
@@ -62,21 +74,19 @@ func TestBuildInfo_MarshalJSON(t *testing.T) {
 		"full": {
 			Version:  DummyVersion,
 			Revision: DummyRevision,
-			Branch:   DummyBranch,
 			Date:     DummyDate,
 		},
 	}
 
-	for name, x := range tests {
+	for name, bld := range tests {
 		t.Run(name, func(t *testing.T) {
-			haveBytes, haveErr := x.MarshalJSON()
+			haveBytes, haveErr := bld.MarshalJSON()
 			wantBytes, wantErr := json.Marshal(struct {
 				Version   string `json:"version"`
-				Revision  string `json:"rev,omitempty"`
-				Branch    string `json:"branch,omitempty"`
+				Revision  string `json:"revision,omitempty"`
 				Date      string `json:"date,omitempty"`
 				GoVersion string `json:"goversion"`
-			}{x.Version, x.Revision, x.Branch, x.Date, x.GoVersion()})
+			}{bld.Version, bld.Revision, bld.Date, bld.GoVersion()})
 
 			assert.Exactly(t, wantBytes, haveBytes)
 			assert.Exactly(t, wantErr, haveErr)
@@ -89,7 +99,6 @@ func TestIsDummy(t *testing.T) {
 		assert.True(t, IsDummy(BuildInfo{
 			Version:  DummyVersion,
 			Revision: DummyRevision,
-			Branch:   DummyBranch,
 			Date:     DummyDate,
 		}))
 	})
