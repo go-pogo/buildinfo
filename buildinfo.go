ad5ccbc9310966550a491ba590150b4930d7266b
roeldev <roeldev@users.noreply.github.com>
2024-02-11 20:04:14 +0100

    Update min go version to 1.20

--- buildinfo.go
+++ buildinfo.go
@@ -9,10 +9,9 @@ import (
 	"github.com/go-pogo/errors"
 	"github.com/go-pogo/writing"
 	"io"
-	"io/fs"
 	"net/http"
-	"os"
 	"runtime"
+	"runtime/debug"
 	"strings"
 	"time"
 )
@@ -60,43 +59,25 @@ type BuildInfo struct {
 	Extra map[string]string
 }
 
-// New creates a new *BuildInfo with the given version string.
+// New creates a new BuildInfo with the given version string.
 func New(ver string) *BuildInfo {
 	bld := &BuildInfo{Version: ver}
 	bld.init()
 	return bld
 }
 
-// Read reads from io.Reader r and json unmarshalls it's content into a new
-// BuildInfo.
-func Read(r io.Reader) (*BuildInfo, error) {
-	var bld BuildInfo
-	bld.init()
+func (bld *BuildInfo) init() {
+	bi, _ := debug.ReadBuildInfo()
+	bld.goVersion = bi.GoVersion
 
-	if err := json.NewDecoder(r).Decode(&bld); err != nil {
-		return nil, errors.WithStack(err)
+	for _, set := range bi.Settings {
+		switch set.Key {
+		case "vcs.revision":
+			bld.Revision = set.Value
+		case "vcs.time":
+			bld.Time, _ = time.Parse(time.RFC3339, set.Value)
+		}
 	}
-
-	return &bld, nil
-}
-
-// Open the file, then read and decode its contents using Read.
-func Open(file string) (*BuildInfo, error) {
-	return OpenFS(os.DirFS(""), file)
-}
-
-// OpenFS opens file from fsys. It then reads and decodes the file's contents
-// using Read.
-func OpenFS(fsys fs.FS, file string) (bld *BuildInfo, err error) {
-	f, err := fsys.Open(file)
-	if err != nil {
-		err = errors.WithStack(err)
-		return nil, err
-	}
-
-	defer errors.AppendFunc(&err, f.Close)
-	bld, err = Read(f)
-	return
 }
 
 const panicReservedKey = "buildinfo: cannot add reserved key "
--- go.mod
+++ go.mod
@@ -1,11 +1,18 @@
 module github.com/go-pogo/buildinfo
 
-go 1.16
+go 1.20
 
 require (
-	github.com/go-pogo/errors v0.8.0
+	github.com/go-pogo/errors v0.9.0
 	github.com/go-pogo/writing v0.1.1
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/stretchr/testify v1.8.2
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
